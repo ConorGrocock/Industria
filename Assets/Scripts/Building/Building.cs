d9c6963ec11213019368f84c1d1a46433a83d0d8
Conor Grocock <11grocockc@utcsheffield.org.uk>
2017-07-29 14:07:18 +0000

    Added House and Building

---
+++ Assets/Scripts/Building/Building.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Building : MonoBehaviour {
+
+    public Sprite sprite;
+
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	protected void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Building/House.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : Building {
+
+    public float babyTime = 60f;
+    [SerializeField]
+    private float timeToNextBaby = 0f;
+
+    [Space(20)]
+    public int capacity;
+    public int maxCapacity = 5;
+    public bool full;
+
+    // Use this for initialization
+    new void Start () {
+        base.Start();
+        timeToNextBaby = babyTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timeToNextBaby <= 0) {
+            timeToNextBaby = babyTime;
+            capacity++;
+        }
+        timeToNextBaby -= Time.deltaTime;
+	}
+}
