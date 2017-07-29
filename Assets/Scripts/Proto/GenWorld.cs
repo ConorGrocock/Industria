8e8fb76e2c53929e7e3562477e2df010e2434621
Rob Bladen <rb2750@live.co.uk>
2017-07-29 12:42:07 +0000

    Fix

--- Assets/Scripts/Proto/GenWorld.cs
+++ Assets/Scripts/Proto/GenWorld.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenWorld : MonoBehaviour {
+public class GenWorld : MonoBehaviour
+{
 
     public int worldHeight = 10;
     public int worldWidth = 10;
     public GameObject Tile;
 
     // Use this for initialization
-    void Start () {
-        for (int i = 0; i < worldHeight; i++) {
-            for (int j = 0; j < worldWidth; j++) {
+    void Start()
+    {
+        for (int i = -worldHeight / 2; i < worldHeight / 2; i++)
+        {
+            for (int j = -worldWidth / 2; j < worldWidth / 2; j++)
+            {
                 GameObject cTile = Instantiate(Tile);
                 Vector3 cTrans = cTile.transform.position;
                 cTrans.x = i * Tile.GetComponent<Renderer>().bounds.size.x;
@@ -19,10 +23,11 @@ public class GenWorld : MonoBehaviour {
                 cTile.transform.position = cTrans;
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
