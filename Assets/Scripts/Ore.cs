b17c51b584e53e9883417ccaf46553a574268b9f
Conor Grocock <11grocockc@utcsheffield.org.uk>
2017-07-30 13:30:32 +0000

    Create ore and ore type

---
+++ Assets/Scripts/Ore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum OreTypes {
+    Coal,
+    Iron,
+    Copper
+}
+
+public class Ore {
+    public OreTypes type;
+    public int amount;
+}
+
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-
+    Ore ore;
     /// <summary>
     /// Building on this tile
     /// </summary>
