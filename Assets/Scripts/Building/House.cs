a24da4679bb67458847d1c78f3a0ec2716a7438d
Conor Grocock <11grocockc@utcsheffield.org.uk>
2017-08-01 00:19:22 +0000

    Added house spirtes

--- Assets/Scripts/Building/House.cs
+++ Assets/Scripts/Building/House.cs
@@ -117,9 +117,10 @@ public class House : Building
             rt.anchorMin = new Vector2(0, 0);
             rt.anchorMax = new Vector2(0, 0);
 
-            profiles[i].transform.localPosition = new Vector3((i * 170) - 275, -100, 1);
+            profiles[i].transform.localPosition = new Vector3((i * 170) - 260, -100, 1);
             profiles[i].GetComponentInChildren<Text>().text = h.occupants[i].Vname;
             profiles[i].GetComponentInChildren<Dropdown>().value = (int)h.occupants[i].role;
+            profiles[i].GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>("Sprites/Villager/" + Random.Range(1, 4));
         }
         shown = h.occupants.Count;
     }
