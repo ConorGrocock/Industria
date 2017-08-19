d1c7d1ca830448b33424d2b9bdfa8dee2c69fe09
Luke Wilde <11wildel@utcsheffield.org.uk>
2017-08-19 20:02:28 +0000

    The mine now has a hover panel

--- Assets/Scripts/Building/Mine.cs
+++ Assets/Scripts/Building/Mine.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mine : Building
 {
-
     /// <summary>
     /// Mining speed
     /// amount of ore to mine per second.
@@ -24,11 +24,37 @@ public class Mine : Building
 
     public GameObject Villager;
 
+    public GameObject hoverPanel;
+    private GameObject hoverPanelInstance;
+
+    private Text oreTypeText;
+
+    private Image personOne;
+    private Text personOneName;
+
+    private Image personTwo;
+    private Text personTwoName;
+
+    private Image personThree;
+    private Text personThreeName;
+
+    private Image personFour;
+    private Text personFourName;
+
+    private Image personFive;
+    private Text personFiveName;
+
+    private Text powerDrawText;
+    private Slider miningProgressSlider;
+
+    private List<Villager> occupants = new List<Villager>();
+
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
         timeSinceLastMine = Time.time;
+        hoverPanel = Resources.Load("Prefabs/UI/MineHoverPanel", typeof(GameObject)) as GameObject;
     }
 
     // Update is called once per frame
@@ -50,14 +76,14 @@ public class Mine : Building
         if (workers != lastWorkers)
         {
             lastWorkers = workers;
-            List<Villager> villagers = new List<Villager>();
+            occupants = new List<Villager>();
             for (int i = 0; i < workers; i++)
             {
                 Villager villager = new Villager();
                 villager.role = VillagerRole.Miner;
-                villagers.Add(villager);
+                occupants.Add(villager);
             }
-            displayHeads(villagers);
+            displayHeads(occupants);
         }
     }
 
@@ -71,6 +97,172 @@ public class Mine : Building
     public override void OnHover()
     {
         base.OnHover();
-        Debug.Log("Mine hover!");
+
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if (hoverPanel != null)
+        {
+            if (hoverPanelInstance == null)
+            {
+                hoverPanelInstance = Instantiate(hoverPanel);
+                hoverPanelInstance.transform.SetParent(canvasTransform);
+
+                oreTypeText = hoverPanelInstance.transform.Find("OreTypeText").gameObject.GetComponent<Text>();
+
+                personOne = hoverPanelInstance.transform.Find("PersonOne").gameObject.GetComponent<Image>();
+                personOneName = personOne.transform.Find("Name").gameObject.GetComponent<Text>();
+
+                personTwo = hoverPanelInstance.transform.Find("PersonTwo").gameObject.GetComponent<Image>();
+                personTwoName = personTwo.transform.Find("Name").gameObject.GetComponent<Text>();
+
+                personThree = hoverPanelInstance.transform.Find("PersonThree").gameObject.GetComponent<Image>();
+                personThreeName = personThree.transform.Find("Name").gameObject.GetComponent<Text>();
+
+                personFour = hoverPanelInstance.transform.Find("PersonFour").gameObject.GetComponent<Image>();
+                personFourName = personFour.transform.Find("Name").gameObject.GetComponent<Text>();
+
+                personFive = hoverPanelInstance.transform.Find("PersonFive").gameObject.GetComponent<Image>();
+                personFiveName = personFive.transform.Find("Name").gameObject.GetComponent<Text>();
+
+                powerDrawText = hoverPanelInstance.transform.Find("PowerDraw").gameObject.GetComponent<Text>();
+                miningProgressSlider = hoverPanelInstance.transform.Find("MiningProgress").gameObject.GetComponent<Slider>();
+            }
+
+            if (tile.ore != null)
+            {
+                switch (tile.ore.type)
+                {
+                    case (OreTypes.Coal):
+                        oreTypeText.text = "Mining: Coal";
+                        break;
+                    case (OreTypes.Copper):
+                        oreTypeText.text = "Mining: Copper";
+                        break;
+                    case (OreTypes.Iron):
+                        oreTypeText.text = "Mining: Iron";
+                        break;
+                    default:
+                        Debug.LogError("[Mine] [OnHover] Unexpected ore type: " + tile.ore.type);
+                        break;
+                }
+            }
+            else
+            {
+                Debug.LogError("[Mine] [OnHover] Mine is not on an ore! (tile.ore is null)");
+            }
+
+            personOne.gameObject.SetActive(true);
+            personTwo.gameObject.SetActive(true);
+            personThree.gameObject.SetActive(true);
+            personFour.gameObject.SetActive(true);
+            personFive.gameObject.SetActive(true);
+
+            hoverPanelInstance.transform.position = new Vector3(Input.mousePosition.x + 115, Input.mousePosition.y - 115);
+
+            switch (occupants.Count)
+            {
+                case (2):
+                    personThree.gameObject.SetActive(false);
+                    personFour.gameObject.SetActive(false);
+                    personFive.gameObject.SetActive(false);
+                    break;
+                case (3):
+                    personFour.gameObject.SetActive(false);
+                    personFive.gameObject.SetActive(false);
+                    break;
+                case (4):
+                    personFive.gameObject.SetActive(false);
+                    break;
+                case (5):
+                    break;
+                default:
+                    Debug.LogError("[Mine] [OnHover] Unexpected occupant count for hover panel: " + occupants.Count);
+                    break;
+            }
+
+            int i = 1;
+
+            foreach (Villager occupant in occupants)
+            {
+                switch (i)
+                {
+                    case (1):
+                        personOne.sprite = occupant.getHead();
+                        personOneName.text = occupant.Vname;
+                        break;
+                    case (2):
+                        personTwo.sprite = occupant.getHead();
+                        personTwoName.text = occupant.Vname;
+                        break;
+                    case (3):
+                        personThree.sprite = occupant.getHead();
+                        personThreeName.text = occupant.Vname;
+                        break;
+                    case (4):
+                        personFour.sprite = occupant.getHead();
+                        personFourName.text = occupant.Vname;
+                        break;
+                    case (5):
+                        personFive.sprite = occupant.getHead();
+                        personFiveName.text = occupant.Vname;
+                        break;
+                    default:
+                        Debug.LogError("[Mine] [OnHover] Unexpected iterator for setting details on hover panel: " + i);
+                        break;
+                }
+
+                i++;
+            }
+
+            powerDrawText.text = "Power Draw: " + powerDraw;
+            miningProgressSlider.value = (Time.time - timeSinceLastMine) / (1.0f / miningSpeed);
+        }
+        else
+        {
+            Debug.LogError("Hover panel is null!");
+        }
+
+        //spriteRenderer.color = new Color(0.2f, 0.7f, 0.3f);
+    }
+
+    public override void OnHoverEnd()
+    {
+        base.OnHoverEnd();
+
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if (hoverPanelInstance != null)
+        {
+            Destroy(hoverPanelInstance);
+            hoverPanelInstance = null;
+        }
+
+        oreTypeText = null;
+
+        personOne = null;
+        personOneName = null;
+
+        personTwo = null;
+        personTwoName = null;
+
+        personThree = null;
+        personThreeName = null;
+
+        personFour = null;
+        personFourName = null;
+
+        personFive = null;
+        personFiveName = null;
+
+        powerDrawText = null;
+        miningProgressSlider = null;
+
+        //spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f);
     }
 }
